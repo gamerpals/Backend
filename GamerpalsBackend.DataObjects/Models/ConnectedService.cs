03823fd7c843e686f79c9d92e2ac706b906ef121
Stefan Gornik <steff@gornik.at>
2019-09-30 21:37:44 +0200

    #24

---
+++ GamerpalsBackend.DataObjects/Models/ConnectedService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+
+namespace GamerPalsBackend.DataObjects.Models
+{
+    public class ConnectedService : IModelBase
+    {
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+    }
+}
